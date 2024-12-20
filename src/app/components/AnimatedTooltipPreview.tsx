03aa8f8e140d068c4a197019a7afd3f2d47fe3e3
Chandan NL <cnl3475@gmail.com>
2024-12-20 20:27:30 +0530

    add our team

--- src/app/components/AnimatedTooltipPreview.tsx
+++ src/app/components/AnimatedTooltipPreview.tsx
@@ -6,37 +6,37 @@ const people = [
         id: 1,
         name: "Chandan N L",
         designation: "Software Engineer",
-        image: "https://ik.imagekit.io/p5dmneaken/photo.webp"
+        image: "https://ik.imagekit.io/p5dmneaken/Team/chandan.jpg?updatedAt=1734705651737"
     },
     {
         id: 2,
         name: "Varun M S",
         designation: "Product Manager",
-        image: "https://ik.imagekit.io/p5dmneaken/photo.webp"
+        image: "https://ik.imagekit.io/p5dmneaken/Team/varun.jpg"
     },
     {
         id: 3,
         name: "Vinod Kumar",
         designation: "Data Scientist",
-        image: "https://ik.imagekit.io/p5dmneaken/photo.webp"
+        image: "https://ik.imagekit.io/p5dmneaken/Team/vinod?updatedAt=1734706462445"
     },
     {
         id: 4,
         name: "Diwakar K",
         designation: "UX Designer",
-        image: "https://ik.imagekit.io/p5dmneaken/photo.webp"
+        image: "https://ik.imagekit.io/p5dmneaken/Team/diwakar?updatedAt=1734706326320"
     },
     {
         id: 5,
         name: "Anand Agarwal",
         designation: "Soap Developer",
-        image: "https://ik.imagekit.io/p5dmneaken/photo.webp"
+        image: "https://ik.imagekit.io/p5dmneaken/Team/anand.jpg"
     },
     {
         id: 6,
         name: "Dora",
         designation: "The Explorer",
-        image: "https://ik.imagekit.io/p5dmneaken/photo.webp"
+        image: "https://ik.imagekit.io/p5dmneaken/Team/dora.jpg"
     },
 ];
 

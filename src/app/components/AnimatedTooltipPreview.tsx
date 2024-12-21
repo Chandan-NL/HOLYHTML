ad94274ec70113d40ec03213d1e08cdac8bb0e00
Chandan NL <cnl3475@gmail.com>
2024-12-21 20:08:37 +0530

    add team roles

--- src/app/components/AnimatedTooltipPreview.tsx
+++ src/app/components/AnimatedTooltipPreview.tsx
@@ -5,31 +5,31 @@ const people = [
     {
         id: 1,
         name: "Chandan N L",
-        designation: "Software Engineer",
+        designation: "Frontend Developer & Team Lead",
         image: "https://ik.imagekit.io/p5dmneaken/Team/chandan.jpg?updatedAt=1734705651737"
     },
     {
         id: 2,
         name: "Varun M S",
-        designation: "Product Manager",
+        designation: "Fullstack Developer",
         image: "https://ik.imagekit.io/p5dmneaken/Team/varun.jpg"
     },
     {
         id: 3,
         name: "Vinod Kumar",
-        designation: "Data Scientist",
+        designation: "Web Developer",
         image: "https://ik.imagekit.io/p5dmneaken/Team/vinod?updatedAt=1734706462445"
     },
     {
         id: 4,
         name: "Diwakar K",
-        designation: "UX Designer",
+        designation: "Project Manager",
         image: "https://ik.imagekit.io/p5dmneaken/Team/diwakar?updatedAt=1734706326320"
     },
     {
         id: 5,
         name: "Anand Agarwal",
-        designation: "Soap Developer",
+        designation: "React Developer",
         image: "https://ik.imagekit.io/p5dmneaken/Team/anand.jpg"
     },
     {
@@ -44,7 +44,6 @@ export function AnimatedTooltipPreview() {
     return (
         <div className="flex flex-row items-center justify-center mt-20 mb-10 w-full">
             <AnimatedTooltip items={people} />
-
         </div>
     );
 }
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
                         width: "10rem"
                     }} />
                 </div>
-                <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2024 HOLYHTML — Designed & Developed
+                <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2024 HOLYHTML — Developed by
                     <a href="https://chandan-nl.github.io/portfolio/" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@Chandan N L</a>
 
                 </p>

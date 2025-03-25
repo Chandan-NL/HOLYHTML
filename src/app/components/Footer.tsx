0f33a5a15a06ce4624c859f8782718939ddde777
Chandan NL <cnl3475@gmail.com>
2025-03-25 14:48:32 +0530

    add animation on page load

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -12,10 +12,9 @@ const Footer = () => {
                     }} />
                 </div>
                 <div className='flex'>
-                <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2024 HOLYHTML — Developed by
-                    <a href="https://chandan-nl.github.io/portfolio/" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@Chandan N L ,</a>
-                </p>
-                <p className='mt-1'>@Varun M S</p>
+                    <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2024 HOLYHTML — Developed by,
+                        <a href="https://chandan-nl.github.io/portfolio/" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@Chandan N L</a>
+                    </p>
                 </div>
                 <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                     <a className="text-gray-500">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Header from "./components/Header";
 import { HeroSection } from "./components/HeroSection";
 import Footer from "./components/Footer";
@@ -5,18 +6,49 @@ import Contact from "./components/Contact";
 import ProjectsSection from "./components/ProjectsSection";
 import ServiceSection from "./components/ServiceSection";
 import NewSection from "./components/NewSection";
+import FadeIn from "./components/FadeIn";
+import { motion } from "framer-motion";
 
 export default function Home() {
+
   return (
     <>
       <main className="bg-white antialiased px-6" aria-hidden="false">
+
         <Header />
-        <HeroSection />
-        <NewSection />
-        <ProjectsSection />
-        <ServiceSection />
-        <Contact />
+
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1.3 }}
+        >
+
+          {/* <FadeIn className="" duration={200} >
+        </FadeIn> */}
+          <FadeIn className="" duration={200} >
+            <HeroSection />
+          </FadeIn>
+
+          <FadeIn className="" duration={200} >
+            <NewSection />
+          </FadeIn>
+
+          <FadeIn className="" duration={200} >
+            <ProjectsSection />
+          </FadeIn>
+
+          <FadeIn className="" duration={200} >
+            <ServiceSection />
+          </FadeIn>
+
+          <FadeIn className="" duration={200} >
+            <Contact />
+          </FadeIn>
+
+        </motion.div>
         <Footer />
+
       </main>
     </>
   );
